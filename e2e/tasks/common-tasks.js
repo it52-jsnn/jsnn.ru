d98a7068fdd4dcafb5be66d74f9bee8e6674576a
Blokhin Andrew <intestinesonastick@gmail.com>
2020-04-29 23:54:11 +0300

    Added TEST-3.spec.js [Main page, Titles]

--- e2e/tasks/common-tasks.js
+++ e2e/tasks/common-tasks.js
@@ -1,11 +1,32 @@
-const { pageUrls } = require('constants.js');
+const { globalTimeouts, pageUrls } = require('constants.js');
 
-const gotoMainPage = (page = global.page) => page.goto(pageUrls.main);
-const gotoAboutPage = (page = global.page) => page.goto(pageUrls.about);
-const gotoEventPage = (eventId, page = global.page) => page.goto(pageUrls.event(eventId));
+const gotoMainPage = (page = global.page) => page.goto(pageUrls.main, { waitUntil: 'domcontentloaded'});
+const gotoAboutPage = (page = global.page) => page.goto(pageUrls.about, { waitUntil: 'domcontentloaded'});
+const gotoEventPage = (eventId, page = global.page) => page.goto(pageUrls.event(eventId), { waitUntil: 'domcontentloaded'});
+
+const click = async (selectorOrHandle, page = global.page) => {
+  const handle = typeof selectorOrHandle === 'string' ? await page.waitForSelector(selectorOrHandle) : selectorOrHandle;
+  await handle.click();
+};
+
+const clickAndWaitForNavigation = async (selectorOrHandle, page = global.page) => {
+  await click(selectorOrHandle, page);
+  await page.waitForNavigation();
+};
+
+const checkLocation = (targetHref, page = global.page) => page.waitForFunction(tHref => {
+
+  return location.href === tHref;
+
+}, { timeout: globalTimeouts.checkValue }, targetHref);
 
 module.exports = {
   gotoMainPage,
   gotoAboutPage,
-  gotoEventPage
+  gotoEventPage,
+
+  click,
+  clickAndWaitForNavigation,
+
+  checkLocation
 };
