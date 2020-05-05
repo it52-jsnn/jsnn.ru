481382001b252cd1e09cecdf4c5edd696dd818de
Blokhin Andrew <intestinesonastick@gmail.com>
2020-05-05 19:57:38 +0300

    Move from Node 12 to Node 13.14.0 in Puppeteer Action

--- e2e/test-run-configuration/create-events-list.js
+++ e2e/test-run-configuration/create-events-list.js
@@ -9,7 +9,7 @@ const parsedData = JSON.parse(md2json.parse(fileNames, {}));
 
 global.EVENTS_LIST = parsedData;
 
-fs.writeFile(path.resolve(__dirname, 'events-list.js'), `
+fs.writeFileSync(path.resolve(__dirname, 'events-list.js'), `
 'This file is for debugging purpose only! You can access EVENTS_LIST from the global.'
 
 const EVENTS_LIST = ${ JSON.stringify(parsedData, null, 2) }
